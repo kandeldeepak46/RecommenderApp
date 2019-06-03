d064b996c9aa556311b77d9863d604c6f7f1c745
JustAyush <salvatorterminator@gmail.com>
2019-06-03 21:32:50 +0545

    trying to add dropdown list

--- users/forms.py
+++ users/forms.py
@@ -2,9 +2,18 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
+FRUIT_CHOICES= [
+    ('orange', 'Oranges'),
+    ('cantaloupe', 'Cantaloupes'),
+    ('mango', 'Mangoes'),
+    ('honeydew', 'Honeydews'),
+    ]
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.Select(choices=FRUIT_CHOICES))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'favorite_fruit']
