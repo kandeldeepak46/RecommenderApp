893e1b58a494f8b7bd785eb7e8e35f35d738bbc2
JustAyush <salvatorterminator@gmail.com>
2019-06-09 10:27:21 +0545

    star rating input added

--- example/views.py
+++ example/views.py
@@ -8,10 +8,14 @@ export = []
 with open('exportNew.json', 'r') as myfile:
     export = json.load(myfile)
 
+bookDetail = ''
 
 
 # Create your views here.
 def index(request):
+
+    global export
+
     if request.user.is_authenticated:
         if (request.user.profile.role == 'Shopkeeper'):
             shopkeeper = 'yes'
@@ -20,23 +24,28 @@ def index(request):
     else: 
         shopkeeper = 'no'
 
-    global export
+    
     return render(request, 'example/index.html', {'e': export, 'shopkeeper': shopkeeper})
 
+
 def detail(request, isbn):
-    if request.user.is_authenticated:
-        if (request.user.profile.role == 'Shopkeeper'):
-            shopkeeper = 'yes'
-        else: 
-            shopkeeper = 'no'
-    else: 
-        shopkeeper = 'no'
 
     global export
+    global bookDetail
 
     for book in export:
         if book['ISBN'] == isbn:
             bookDetail = book
+
+    if request.user.is_authenticated:
+        if (request.user.profile.role == 'Shopkeeper'):
+            shopkeeper = 'yes'
+        else: 
+            shopkeeper = 'no'
+    else: 
+        shopkeeper = 'no'
+
+    
     
     return render(request, 'example/detail.html', {'b': bookDetail, 'e': export, 'shopkeeper': shopkeeper})
 
