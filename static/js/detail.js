fbe18473dd52e0355a65b1de174261eead09b545
JustAyush <salvatorterminator@gmail.com>
2019-06-15 19:25:39 +0545

    about to add fifteenBooks flag

--- static/js/detail.js
+++ static/js/detail.js
@@ -66,3 +66,37 @@ const ratings = {
       // Add number rating
       // document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
     }
+
+
+    function sendReadIt(){
+      console.log(isbn);
+      fetch("detail_url", {
+        method: "POST", 
+        credentials: "same-origin",
+        headers: {
+            "X-CSRFToken": getCookie("csrftoken"),
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify({readIt: 'read', clicked: 'clicked'})
+      }).then(res => {
+        console.log("Request complete! response:", res);
+      })
+    }
+
+    function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+    }
--- users/models.py
+++ users/models.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=100)
+
